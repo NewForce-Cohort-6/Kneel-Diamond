2ed6a1a401f7e4bd9bd1feca0dc5449db514b365
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-07-19 13:49:00 -0400

    click event started

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -1,11 +1,14 @@
 import { Metals } from "./Metals.js"
 import { DiamondSizes } from "./DiamondSizes.js"
 import { JewelryStyles } from "./JewelryStyles.js"
-import { Orders } from "./Orders.js"
+import { addCustomOrder } from "./database.js"
 
 document.addEventListener(
     "click",
     (event) => {
+        if(event.target.id === "orderButton"){
+            addCustomOrder()
+        }
     }
 )
 
